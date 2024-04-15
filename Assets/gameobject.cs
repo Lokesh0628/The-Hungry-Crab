d1357e2609d99a65b840eb9d0b1fcbdfe89db79e
Lokesh <lokeshbabu944@gmail.com>
2024-04-14 20:33:36 -0400

    signup complete

---
+++ Assets/gameobject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour
+{
+    public GameObject prefab;
+
+    // Instantiate the Prefab somewhere between -10.0 and 10.0 on the x-z plane
+    void Start()
+    {
+        Vector3 position = new Vector3(Random.Range(-13.3f, 11.4f), 0, Random.Range(14.4f, -9f));
+        Instantiate(prefab, position, Quaternion.identity);
+    }
+}
+
---
+++ Assets/moment.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    public class moment : MonoBehaviour
+    {
+        public float moveSpeed = 3f;
+        
+        void Start()
+        {
+
+        }
+
+        
+        void Update()
+        {
+           
+            transform.Translate(Vector3.up * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+                       transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+        }
+    }
